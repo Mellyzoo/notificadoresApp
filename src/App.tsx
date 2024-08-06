1e85e9410ef546c02acf23a2bfd18802b1bc01f3
lean <mellyleo@hotmail.com>
2024-08-06 20:22:01 -0300

    commit modelado de DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import "./App.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import React from "react";
+import Container from "./components/Container"
+
 
 export default function App() {
-	return (		
-			<MapContainer
-				center={[-33.3604, -61.4713]}
-				zoom={8}
-				style={{ height: "100vh", width: "100vw" }}
-			>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<Marker position={[-33.2743, -60.7127]}>
-					<Popup>
-						El primer marcador sobre Rosario <br /> Hay que customizarlo.
-					</Popup>
-				</Marker>
-			</MapContainer>
-	);
+	return<Container/>;
 }
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,6 +1,73 @@
-// interface Position {
-//   latitude: number;
-//   longitude: number;
-//   timestamp: number;
-// }
-// export default Position
+interface Localizacion {
+	latitud: number;
+	longitud: number;
+	timestamp?: number;
+}
+
+interface Direccion {
+	calle: string;
+	numero: number;
+	localidad: string;
+}
+
+interface Plan {
+	planId: number;
+	direccion?: Direccion;
+	localizacion: Localizacion;
+	area?: GeoJSONLayer;
+	cod_postal: number;
+	edificios?: Edificio[];
+	viviendas?: Vivienda[];
+}
+
+interface Vivienda {
+	id: number | string;
+	cuenta: number;
+	planId: number;
+	direccion: Direccion;
+	localizacion?: Localizacion;
+}
+
+interface Edificio {
+	id: number | string;
+	direccion: Direccion;
+	planId: number;
+	localizacion: Localizacion;
+	departamentos: Departamento[];
+	numPisos?: number;
+}
+
+interface Departamento {
+	id: number | string;
+	cuenta: number;
+	edificioId: number | string;
+	localizacion: Localizacion;
+}
+
+// Definiciones de tipos GeoJSON
+type GeoJSONType = "Feature" | "FeatureCollection";
+
+interface GeoJSONGeometry {
+	type:
+		| "Point"
+		| "LineString"
+		| "Polygon"
+		| "MultiPoint"
+		| "MultiLineString"
+		| "MultiPolygon"
+		| "GeometryCollection";
+	coordinates: number[] | number[][] | number[][][] | number[][][][];
+}
+
+interface GeoJSONFeature {
+	type: "Feature";
+	geometry: GeoJSONGeometry;
+	properties: { [key: string]: any };
+}
+
+interface GeoJSONFeatureCollection {
+	type: "FeatureCollection";
+	features: GeoJSONFeature[];
+}
+
+type GeoJSONLayer = GeoJSONFeature | GeoJSONFeatureCollection;
