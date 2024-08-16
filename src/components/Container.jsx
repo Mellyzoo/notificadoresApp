7c19667c27c76140344cd6c205c6820d3565fefe
lean <mellyleo@hotmail.com>
2024-08-15 23:33:59 -0300

    new toolbar component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,22 +1,29 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { LayersControl } from "react-leaflet";
-import { FindMyPosition } from "./FindMyPosition";
+// import { FindMyPosition } from "./FindMyPosition";
+import { Toolbar } from "./Toolbar";
 
 export default function Container() {
 	return (
-		<MapContainer
-			center={[-33.3604, -61.4713]}
-			zoom={16}
-			scrollWheelZoom={false}
-			style={{ height: "100vh", width: "100vw" }}
+		<div
+			className="main-container"
+			style={{ display: "flex", flexDirection: "column" }}
 		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<FindMyPosition />
-			<LayersControl />
-		</MapContainer>
+			<MapContainer
+				center={[-33.3604, -61.4713]}
+				zoom={16}
+				scrollWheelZoom={true}
+				style={{ height: "90vh", width: "100vw" }}
+			>
+				<LayersControl />
+				{/* <FindMyPosition /> */}
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+			</MapContainer>
+			<Toolbar />
+		</div>
 	);
 }
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { MapContainer } from "react-leaflet";
+import ImagePosition from "../assets/find.png";
+import { FindMyPosition } from "./FindMyPosition";
+
+export function Toolbar() {
+	return (
+		<div style={{ height: "10vh", width: "100vw", backgroundColor: "red" }}>
+			<FindMyPosition />
+			<img
+				style={{ width: "10px", height: "10px" }}
+				src={ImagePosition}
+				alt="find"
+			/>
+		</div>
+	);
+}
