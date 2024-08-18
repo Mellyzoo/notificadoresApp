d3c339c01f8970cbb6c99edc115c76799f0dc01b
lean <mellyleo@hotmail.com>
2024-08-18 10:50:54 -0300

    deploy 11 pm

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,74 +1,79 @@
-interface Localizacion {
-	latitud: number;
-	longitud: number;
-	timestamp?: number;
-}
+// interface Localizacion {
+// 	latitud: number;
+// 	longitud: number;
+// 	timestamp?: number;
+// }
 
-interface Direccion {
-	calle: string;
-	numero: number;
-	localidad: string;
-}
+// interface Direccion {
+// 	calle: string;
+// 	numero: number;
+// 	localidad: string;
+// }
 
-interface Plan {
-	planId: number;
-	localizacion: Localizacion;
-	area?: GeoJSONLayer;
-	cod_postal: number;
-	edificios?: Edificio[];
-	viviendas?: Vivienda[];
-}
+// interface Plan {
+// 	planId: number;
+// 	localizacion: Localizacion;
+// 	area?: GeoJSONLayer;
+// 	cod_postal: number;
+// 	edificios?: Edificio[];
+// 	viviendas?: Vivienda[];
+// }
 
-interface Vivienda {
-	id: number | string;
-	cuenta?: number;
-	planId: number;
-	direccion: Direccion;
-	localizacion?: Localizacion;
-}
 
-interface Edificio {
-	id: number | string;
-	direccion: Direccion;
-	planId: number;
-	localizacion: Localizacion;
-	departamentos: Departamento[];
-	numPisos?: number;
-}
+// interface Vivienda {
+// 	id: number | string;
+// 	cuenta?: number;
+// 	planId: number;
+// 	direccion: Direccion;
+// 	localizacion?: Localizacion;
+// }
 
-interface Departamento {
-	id: number | string;
-	cuenta?: number;
-	planId: number;
-	edificioId: number | string;
-	localizacion: Localizacion;
-}
+// interface Edificio {
+// 	id: number | string;
+// 	direccion: Direccion;
+// 	planId: number;
+// 	localizacion: Localizacion;
+// 	departamentos: Departamento[];
+// 	numPisos?: number;
+// }
 
-// Definiciones de tipos GeoJSON
-type GeoJSONType = "Feature" | "FeatureCollection";
+// interface Departamento {
+// 	id: number | string;
+// 	cuenta?: number;
+// 	planId: number;
+// 	edificioId: number | string;
+// 	localizacion: Localizacion;
+// }
 
-interface GeoJSONGeometry {
-	type:
-		| "Point"
-		| "LineString"
-		| "Polygon"
-		| "MultiPoint"
-		| "MultiLineString"
-		| "MultiPolygon"
-		| "GeometryCollection";
-	coordinates: number[] | number[][] | number[][][] | number[][][][];
-}
+// // Definiciones de tipos GeoJSON
 
-interface GeoJSONFeature {
-	type: "Feature";
-	geometry: GeoJSONGeometry;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	properties: { [key: string]: any };
-}
 
-interface GeoJSONFeatureCollection {
-	type: "FeatureCollection";
-	features: GeoJSONFeature[];
-}
+// //comente porque el linter me mostraba error
+// // type GeoJSONType = "Feature" | "FeatureCollection";
 
-type GeoJSONLayer = GeoJSONFeature | GeoJSONFeatureCollection;
+// interface GeoJSONGeometry {
+// 	type:
+// 		| "Point"
+// 		| "LineString"
+// 		| "Polygon"
+// 		| "MultiPoint"
+// 		| "MultiLineString"
+// 		| "MultiPolygon"
+// 		| "GeometryCollection";
+// 	coordinates: number[] | number[][] | number[][][] | number[][][][];
+// }
+
+// interface GeoJSONFeature {
+// 	type: "Feature";
+// 	geometry: GeoJSONGeometry;
+// 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// 	properties: { [key: string]: any };
+// }
+
+// interface GeoJSONFeatureCollection {
+// 	type: "FeatureCollection";
+// 	features: GeoJSONFeature[];
+// }
+
+// type GeoJSONLayer = GeoJSONFeature | GeoJSONFeatureCollection;
+
