b1e1d6da08c083071e8fdf0164ee535dd7a46b0d
lean <mellyleo@hotmail.com>
2024-08-22 20:02:42 -0300

    delete toolbar content

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { MapContainer } from "react-leaflet";
-import ImagePosition from "../assets/find.png";
-// import { FindMyPosition } from "./FindMyPosition";
+// import { MapContainer } from "react-leaflet";
+
 
 export function Toolbar() {
 	return (
 		<div style={{ height: "10vh", width: "100vw", backgroundColor: "red" }}>
-			{/* <FindMyPosition /> */}
-			<img
-				style={{ width: "10px", height: "10px" }}
-				src={ImagePosition}
-				alt="find"
-			/>
+			
 		</div>
 	);
 }
