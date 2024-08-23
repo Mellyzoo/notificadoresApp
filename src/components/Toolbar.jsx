16aa9adfd8a1501386a3ed2cc1088a0b5770d539
lean <mellyleo@hotmail.com>
2024-08-22 23:29:26 -0300

    config biome

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 // import { MapContainer } from "react-leaflet";
 
-
 export function Toolbar() {
 	return (
-		<div style={{ height: "10vh", width: "100vw", backgroundColor: "red" }}>
-			
-		</div>
+		<div style={{ height: "10vh", width: "100vw", backgroundColor: "red" }} />
 	);
 }
